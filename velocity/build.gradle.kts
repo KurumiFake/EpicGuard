54838bdc6ea5319be09a4f8d7d0f63e4c527d23f
KurumiFake <kurumiisshidowife@gmail.com>
2022-08-03 06:04:11 +0000

    fix skill issue

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -5,8 +5,8 @@ dependencies {
     implementation("mysql:mysql-connector-java:8.0.29")
     implementation("org.xerial:sqlite-jdbc:3.36.0.3")
 
-    compileOnly("com.velocitypowered:velocity-api:3.1.2")
-    annotationProcessor("com.velocitypowered:velocity-api:3.1.2")
+    compileOnly("com.velocitypowered:velocity-api:3.1.1")
+    annotationProcessor("com.velocitypowered:velocity-api:3.1.1")
 }
 
 tasks {
